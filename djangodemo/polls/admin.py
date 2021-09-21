f490ca3e9776a40fa2ba30c27ed80ca042a8baf3
jagruti <chaudharijagruti2@gmail.com>
2021-09-21 13:12:40 +0530

    table added

--- djangodemo/polls/admin.py
+++ djangodemo/polls/admin.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
 
-from .models import Question, Employee
+from .models import Question, Employee, Choice, Project
 
 admin.site.register(Question)
 admin.site.register(Employee)
+admin.site.register(Choice)
+admin.site.register(Project)
